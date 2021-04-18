d71ce04f87b913c17417ce39509f7b291dbb16c8
Code With Mercy <64808015+mercyharbo@users.noreply.github.com>
2021-04-18 18:28:56 +0100

    Add files via upload

---
+++ JS/drumkit.js
@@ -0,0 +1,68 @@
+let numberOfDrumButtons = document.querySelectorAll(".drum").length;
+
+for ( i = 0; i < numberOfDrumButtons; i++) {
+    document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+        const buttonInnerHTML = this.innerHTML;
+
+        makeSound(buttonInnerHTML);
+
+        buttonAnimation(buttonInnerHTML);
+    })
+}
+
+document.addEventListener("keypress", (e) => {
+    makeSound(e.key);
+
+    buttonAnimation(e.key);
+})
+
+function makeSound(key) {
+     switch (key) {
+            case "w":
+                const tom1 = new Audio("sounds/tom-1.mp3");
+                tom1.play();
+                break;
+            
+            case "a":
+                const tom2 = new Audio("sounds/tom-2.mp3");
+                tom2.play();
+                break;
+            
+            case "s":
+                const tom3 = new Audio("sounds/tom-3.mp3");
+                tom3.play();
+                break;
+            
+            case "d":
+                const tom4 = new Audio("sounds/tom-4.mp3");
+                tom4.play();
+                break;
+            
+            case "j":
+                const snare = new Audio("sounds/snare.mp3");
+                snare.play();
+                break;
+            
+            case "k":
+                const crash = new Audio("sounds/crash.mp3");
+                crash.play();
+                break;
+            
+            case "l":
+                const kick = new Audio("sounds/kick-bass.mp3");
+                kick.play();
+                break;
+            
+            default:
+                console.log(buttonInnerHTML)
+        }
+}
+
+function buttonAnimation(currentKey) {
+    const activeButton = document.querySelector("." + currentKey);
+    activeButton.classList.add("pressed");
+
+    setTimeout(() => {
+        activeButton.classList.remove("pressed");
+    }, 100)
+}
